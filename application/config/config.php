c73ddd365c0acc3f66be30f6c6940d1cadd8918c
Tanju Yildiz <ben@tanjuyildiz.com>
2023-04-04 22:07:55 +0300

    ⚡ Updated application's config file

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
+$config['base_url'] .= "://" . $_SERVER['HTTP_HOST'];
+$config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +37,7 @@ $config['base_url'] = '';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
@@ -225,7 +227,11 @@ $config['allow_get_array'] = TRUE;
 | your log files will fill up very fast.
 |
 */
-$config['log_threshold'] = 0;
+if (ENVIRONMENT === 'development' || ENVIRONMENT === 'testing') {
+	$config['log_threshold'] = array(1, 2);
+} else {
+	$config['log_threshold'] = 0;
+}
 
 /*
 |--------------------------------------------------------------------------
